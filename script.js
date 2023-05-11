4ee26ff91f56638e40fcfbd0c298125158ed6c11
Leandro wilker <leandrowilker99@gmail.com>
2023-05-10 21:50:16 -0300

    add form

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrir(){
+    document.querySelector('.form-email').style.opacity = 1
+    document.querySelector('.container-prncipal').style.opacity = 0.3
+}
+
+function volta() {
+    document.querySelector('.form-email').style.opacity = 0
+    document.querySelector('.container-prncipal').style.opacity = 1
+}
